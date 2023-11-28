3dc4b00e48fd9078ed465dfdcbd9ae86ac81bb4e
JoshBergman <111944876+JoshBergman@users.noreply.github.com>
2023-11-28 16:42:56 -0600

    String iteration and keyword system

---
+++ gatherKeywords.ts
@@ -0,0 +1,40 @@
+const is_relevant_keyword = (keyword: string): boolean => {
+  const word = keyword.toLowerCase();
+
+  switch (word) {
+    case "mobi":
+    case "firefox":
+    case "seamonkey":
+    case "chrome":
+    case "chromium":
+    case "safari":
+    case "opr":
+    case "opera":
+      return true;
+    default:
+      return false;
+  }
+};
+
+const isLetter = (str: string) => {
+  return str.length === 1 && /[a-z]/i.test(str);
+};
+
+export const gatherKeywords = (user_agent_string: string) => {
+  let currentWord = "";
+  const keywords: string[] = [];
+
+  for (let i = 0; i < user_agent_string.length; i++) {
+    const currentLetter = user_agent_string[i];
+    if (isLetter(currentLetter)) {
+      currentWord += user_agent_string[i];
+      if (is_relevant_keyword(currentWord)) {
+        keywords.push(currentWord);
+        currentWord = "";
+      }
+    } else {
+      currentWord = "";
+    }
+  }
+  return keywords;
+};
--- parser.ts
+++ parser.ts
@@ -1,2 +1,20 @@
-const james: string = "ruf";
-console.log(james);
+// DATA LOCATIONS:
+// All KEYWORDS are stored in gatherKeywords.ts, hardcoded into the switch case.
+// Keyword combinations to BROWSER IDENTIFICATIONS is in identify.ts
+
+// Intended plan:
+// 1. get an array of keywords relevant to identifying the browser
+// 3. check for the "Mobi" string -> device type (remove any possible Mobi from keywords as well)
+// 2. compare keywords to most likely browser -> browser
+
+import { gatherKeywords } from "./gatherKeywords";
+
+const parse_user_agent = (user_agent_string: string) => {
+  const keywords = gatherKeywords(user_agent_string);
+  return keywords;
+};
+console.log(
+  parse_user_agent(
+    "Mozilla/5.0 (Linux; U; Android 4.0.3; de-ch; HTC Sensation Build/IML74K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"
+  )
+);
